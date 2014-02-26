1d5216aa7098ad9d35163c182881e67563aa2b19
Antoine Colmard <a.colmard@gmail.com>
2014-02-26 09:53:20 +0100

    Ajout expression régulière

--- parser_cpp/parser_cpp/HeaderParser.cpp
+++ parser_cpp/parser_cpp/HeaderParser.cpp
@@ -1,6 +1,5 @@
 #include "HeaderParser.h"
 
-
 HeaderParser::HeaderParser(const std::string &filename)
 {
     load(filename);
@@ -8,5 +7,13 @@ HeaderParser::HeaderParser(const std::string &filename)
 
 void HeaderParser::load(const std::string &filename)
 {
+    std::fstream filestream(filename);
 
+    /*
+     LE SAINT GRALL !
+    (\s*)(const|virtual|static|inline)?(\s*)(\w|\<\w+\>|::)+((\s(&|\*))|((&|\*)\s)|\s)(\w)+\((\w|\s|,|&|\*|\<\w+\>|::)*\)(\s*)(const)?
+    */
+    if (filestream.good())
+    {
+    }
 }
--- parser_cpp/parser_cpp/HeaderParser.h
+++ parser_cpp/parser_cpp/HeaderParser.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <string>
+#include <fstream>
+
+#include "MethodsDatabase.h"
 
 class HeaderParser
 {
 private:
     void load(const std::string & filename);
+
+
+    MethodsDatabase m_Database;
 public:
     HeaderParser(const std::string & filename);
 };
